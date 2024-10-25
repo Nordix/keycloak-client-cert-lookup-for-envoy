89bdddfa8696b9d6ed4222f6f19763608695d688
Tero Saarni <tero.saarni@est.tech>
2024-10-25 23:20:38 +0300

    Better logging
    Signed-off-by: Tero Saarni <tero.saarni@est.tech>

--- src/test/java/io/github/nordix/keycloak/services/x509/DockerComposeExtension.java
+++ src/test/java/io/github/nordix/keycloak/services/x509/DockerComposeExtension.java
@@ -8,20 +8,23 @@
  */
 package io.github.nordix.keycloak.services.x509;
 
-import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
+import java.io.File;
 
-import java.nio.file.Path;
-
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
+import org.apache.commons.exec.ExecuteResultHandler;
 import org.jboss.logging.Logger;
 import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
 
 /**
  * JUnit extension for starting and stopping Docker Compose.
  */
 public class DockerComposeExtension implements BeforeAllCallback, AfterAllCallback {
 
-    private static final String DOCKER_COMPOSE_UP = "docker compose up --force-recreate --detach";
+    private static final String DOCKER_COMPOSE_UP = "docker compose up --force-recreate --no-color --abort-on-container-exit";
     private static final String DOCKER_COMPOSE_DOWN = "docker compose down";
 
     private static Logger logger = Logger.getLogger(DockerComposeExtension.class);
@@ -32,27 +35,55 @@ public class DockerComposeExtension implements BeforeAllCallback, AfterAllCallba
         this.basePath = basePath;
     }
 
+    /**
+     * Start Docker Compose before all tests but do not wait for it to complete.
+     * That allows the logs to be displayed in the console in parallel with the test execution.
+     */
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
-        run(DOCKER_COMPOSE_UP, "Failed to start Docker Compose.");
-        logger.info("To see logs, run 'docker compose logs -f'");
+        run(DOCKER_COMPOSE_UP, false, "Failed to start Docker Compose.");
     }
 
+    /**
+     * Stop Docker Compose after all tests.
+     */
     @Override
     public void afterAll(ExtensionContext context) throws Exception {
-        run(DOCKER_COMPOSE_DOWN, "Failed to stop Docker Compose.");
+        run(DOCKER_COMPOSE_DOWN, true, "Failed to stop Docker Compose.");
     }
 
-    private void run(String command, String errorMessage) throws Exception {
-        ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
-        processBuilder.inheritIO();
-        processBuilder.directory(Path.of(basePath).toFile());
+    /**
+     * Run a command on a subprocess.
+     *
+     * Note:
+     * Use apache-commons-exec since ProcessBuilder had problems with output redirection
+     * (output stopped in the middle even if the process was still running).
+     */
+    private void run(String command, boolean waitForCompletion, String errorMessage) throws Exception {
+        CommandLine cmdLine = CommandLine.parse(command);
+        DefaultExecutor executor = DefaultExecutor.builder().setWorkingDirectory(new File(basePath)).get();
 
-        logger.infov("Running command \"{0}\" in directory \"{1}\"", command, processBuilder.directory());
-        Process process = processBuilder.start();
-        int exitCode = process.waitFor();
-        if (exitCode != 0) {
-            throw new IllegalStateException(errorMessage + " Exit code: " + exitCode);
+        logger.infov("Running command \"{0}\" in directory \"{1}\"", command, executor.getWorkingDirectory());
+
+        if (waitForCompletion) {
+            int exitValue = executor.execute(cmdLine);
+            if (exitValue != 0) {
+                throw new Exception(errorMessage);
+            }
+        } else {
+            executor.execute(cmdLine, new ExecuteResultHandler() {
+                @Override
+                public void onProcessComplete(int exitValue) {
+                    if (exitValue != 0) {
+                        logger.error(errorMessage);
+                    }
+                }
+
+                @Override
+                public void onProcessFailed(ExecuteException e) {
+                    logger.error(errorMessage, e);
+                }
+            });
         }
     }
 }
